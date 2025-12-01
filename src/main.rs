c6f7172fd522c6ab9845538d90449a029a65e083
Kenichi Shintome <tome119@gmail.com>
2025-12-01 11:30:53 +0900

    FizzBuzz問題のとりあえず実装

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub fn fizz_buzz(_: i32) -> String {
-    return "Fizz".to_string();
+pub fn fizz_buzz(num: i32) -> String {
+    // 3でも5でも割り切れるときはFizzBuzzをかえす
+    if (num % 3 == 0) && (num % 5 == 0) {
+        return "FizzBuzz".to_string();
+    }
+
+    // 3で割り切れるときはFizzを返す
+    if num % 3 == 0 {
+        return "Fizz".to_string();
+    }
+
+    // 5で割り切れるときはBuzzを返す
+    if num % 5 == 0 {
+        return "Buzz".to_string();
+    }
+
+    return num.to_string();
 }
+
 // Fizzbuzz問題
 // 1. 数が3で割り切れる時、「Fizz」を出力する。
 // 2. 数が5で割り切れる時、「Buzz」を出力する。
@@ -15,8 +31,24 @@ pub fn fizz_buzz(_: i32) -> String {
 #[allow(non_snake_case)]
 mod tests {
     use super::*;
+
     #[test]
     fn 数が3で割り切れる時_Fizzを出力する() {
         assert_eq!(fizz_buzz(3), "Fizz");
+    }
+
+    #[test]
+    fn 数が5で割り切れる時_Buzzを出力する() {
+        assert_eq!(fizz_buzz(5), "Buzz");
+    }
+
+    #[test]
+    fn 数が3でも5でも割り切れる時_FizzBuzzを出力する() {
+        assert_eq!(fizz_buzz(15), "FizzBuzz");
+    }
+
+    #[test]
+    fn 数が3でも5でも割り切れない場合_そのまま数値を出力する() {
+        assert_eq!(fizz_buzz(7), "7")
     }
 }
